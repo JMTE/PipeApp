b7d8a3f17526962703e78b9a4bb13f338018fd1a
JMTE <JMTEdeveloper@outlook.es>
2021-08-18 13:10:27 +0200

    Trabajamos con el theme escogido en PrimeNG y trasladamos valores como var en el css de style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PipesApp';
+
+  nombre:string="Jose"
+
+
+  cambiarNombre(){
+
+     
+      console.log(this.nombre);
+      
+  }
 }
