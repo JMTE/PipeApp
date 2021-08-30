e33f4dc617f8b90d9e91f93b6e5c30b3b421d04b
JMTE <JMTEdeveloper@outlook.es>
2021-08-30 11:54:06 +0200

    Se cambia la configuración local para utilizar el idioma deseado en los pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {  NgModule } from '@angular/core';
+import {  LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,6 +10,14 @@ import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
 
+//Cambiar la configuracion local de la app
+
+import localeEs from "@angular/common/locales/es"
+
+import {registerLocaleData} from "@angular/common"
+
+registerLocaleData(localeEs);
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,12 @@ import { VentasModule } from './ventas/ventas.module';
   imports: [
     BrowserModule, SharedModule,AppRouterModule, VentasModule
   ],
-  providers: [],
+  providers: [
+
+    {
+      provide:LOCALE_ID, useValue:"es"
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
